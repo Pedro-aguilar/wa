39bc0e03dd435bc17132964b9032c5fc7689bfba
Pedro-aguilar <pedroaguilar100@aragon.unam.mx>
2019-09-27 04:32:21 -0500

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:wa/llamadas.dart';
+import 'package:wa/camara.dart';
+import 'package:wa/chats.dart';
+import 'package:wa/estados.dart';
 
-main() => runApp(Vista());
+void main()=>runApp(new WhatsApp());
 
-class Vista extends StatelessWidget {
+class WhatsApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.green),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(title: Text('Vistas')),
-            body: ListView(
-              children: <Widget>[
-                Card(
-                  child: ListTile(
-                    leading: Image.asset(
-                      'assets/images/1.jpg',
-                      width: 50,
-                    ),
-                    title: Text('Titulo'),
-                    subtitle: Text(
-                        'Este es un subtitulo de un list view Este es un subtitulo de un list view  Este es un subtitulo de un list view '),
-                    trailing: Icon(Icons.thumb_up),
-                    isThreeLine: true,
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Image.asset(
-                      'assets/images/2.jpg',
-                      width: 50,
-                    ),
-                    title: Text('Titulo'),
-                    subtitle: Text(
-                        'Este es un subtitulo de un list view Este es un subtitulo de un list view  Este es un subtitulo de un list view '),
-                    trailing: Icon(Icons.thumbs_up_down),
-                    isThreeLine: true,
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Image.asset(
-                      'assets/images/3.jpg',
-                      width: 50,
-                    ),
-                    title: Text('Titulo'),
-                    subtitle: Text(
-                        'Este es un subtitulo de un list view Este es un subtitulo de un list view  Este es un subtitulo de un list view '),
-                    trailing: Icon(Icons.thumb_down),
-                    isThreeLine: true,
-                  ),
-                )
-              ],
-            )));
+    // TODO: implement build
+    return new MaterialApp(
+      title: "Mi WhatsApp",
+      theme:new ThemeData(
+        primaryColor:new Color(0xff075e54),
+        accentColor: new Color(0xff25d366),
+      ),
+      debugShowCheckedModeBanner: false,
+      home:new WhatsAppHome(),
+    );
+  }
+}
+class WhatsAppHome extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new WhatsAppState();
+  }
+}
+class WhatsAppState extends State<WhatsAppHome> with SingleTickerProviderStateMixin{
+
+  TabController t;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    t=new TabController(length: 4, vsync: this,initialIndex: 1);
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        actions: <Widget>[
+          new Icon(Icons.search),
+          new Padding(padding: EdgeInsets.only(right: 5.0)),
+          new Icon(Icons.more_vert),
+        ],
+        bottom: new TabBar(
+          controller: t,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: Icon(Icons.camera_alt),),//Camera
+            new Tab(text: "Chats"),//Chat
+            new Tab(text: "Estados",),//Status
+            new Tab(text: "Lamadas",),//Calls
+          ],
+
+        ),
+      ),
+      body: new TabBarView(
+        controller: t,
+          children:<Widget>[
+            new Camara(),
+            new Chats(),
+            new Estados(),
+            new Llamadas(),
+          ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(Icons.message,color: Colors.white,),
+      ),
+
+    );
   }
 }
---
+++ lib/modelo.dart
@@ -0,0 +1,13 @@
+class Modelo{
+  String name,message,time;
+  Modelo(this.name, this.message, this.time);
+}
+List<Modelo> details=[
+  new Modelo("amigo 1", "hola", "1:30"),
+  new Modelo("amigo 2", "hey", "1:30"),
+  new Modelo("amigo 3", "hi", "1:30"),
+  new Modelo("amigo 4", "adios", "1:30"),
+  new Modelo("amigo 5", "gracias", "1:30"),
+  
+
+];
